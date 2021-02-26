3a84fc0f50aaffe95cfe80bad65bf4e1ddb5d90a
Luis Ramos <luismeramosr@gmail.com>
2021-02-25 21:50:14 -0500

    Agregando cambios al angular.json para produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import {ApiModule} from './core/api/api.module';
 import {ApiInterceptor} from './core/api-interceptor/ApiInterceptor';
 import {environment} from 'src/environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 
 // configuracion de headers global para la api
@@ -24,7 +25,8 @@ export const API_INTERCEPTOR_PROVIDER: Provider = {
         HttpClientModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        ApiModule.forRoot({rootUrl: environment.apiURL})
+        ApiModule.forRoot({rootUrl: environment.apiURL}),
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
         // SharedModule,
         // CoreModule,
         // ProductModule,
